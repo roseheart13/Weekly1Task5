8e9aec5ed37b6e1b4a769c4ead030a484db9b829
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-06 16:10:15 +0200

    Add project files.

---
+++ Weekly1Task5/Weekly1Task5.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "     1   2   3   4   5   6" << std::endl;
+    std::cout << "    --- --- --- --- --- ---" << std::endl;
+    std::cout << " A : * : * : * : * : * : * : A " << std::endl;
+    std::cout << "    --- --- --- --- --- --- " << std::endl;
+    std::cout << " B : * : * : * : * : * : * : B " << std::endl;
+    std::cout << "    --- --- --- --- --- --- " << std::endl;
+    std::cout << " C : * : * : * : * : * : * : C " << std::endl;
+    std::cout << "    --- --- --- --- --- --- " << std::endl;
+    std::cout << " D : * : * : * : * : * : * : D " << std::endl;
+    std::cout << "    --- --- --- --- --- --- " << std::endl;
+    std::cout << "     1   2   3   4   5   6" << std::endl;
+}
+
